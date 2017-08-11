18243381d6ed1c3562db56a90f494fce12359a71
yc5 <yc@xstar.me>
2017-08-11 16:55:00 +0800

    A huge bug of TKU system

--- addcourse_button_2018.js
+++ addcourse_button_2018.js
@@ -1,6 +1,6 @@
 javascript:
 if (window.location.pathname.indexOf("KeyinOver") !== -1) {
-  document.getElementById("btnReLogin").click();
+  window.location.assign("http://www.ais.tku.edu.tw/EleCos/action.aspx");
 } else {
   var d = document.form1;
   d.txtCosEleSeq.value = "<<The Course Code>>";
